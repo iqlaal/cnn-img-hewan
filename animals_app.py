82e07b0393a7d57867df5d110116cba0fc04eb30
iqlaal <129346593+iqlaal@users.noreply.github.com>
2025-01-09 03:00:06 +0700

    Update animals_app.py

--- animals_app.py
+++ animals_app.py
@@ -225,15 +225,13 @@ st.markdown("""
 - <span title="🐻‍❄️ Burrowing marsupials with strong claws.">**wombat**</span>
 """, unsafe_allow_html=True)
 
-# Data for Animals performance
+# Data for Animals performance (mengambil semua nama dari class_names)
 data = {
-    "Class": [
-        "rhinoceros", "lion", "octopus", "oyster", "pigeon",
-        "snake", "tiger", "woodpecker", "jellyfish", "lizard"
-    ],
-    "Accuracy": [0.91, 0.88, 0.85, 0.78, 0.83, 0.79, 0.87, 0.82, 0.86, 0.81],
-    "Precision": [0.89, 0.85, 0.84, 0.76, 0.80, 0.77, 0.86, 0.79, 0.83, 0.78]
+    "Class": class_names,
+    "Accuracy": [0.91, 0.88, 0.85, 0.78, 0.83, 0.79, 0.87, 0.82, 0.86, 0.81, 0.90, 0.84, 0.87, 0.80, 0.79, 0.85, 0.83, 0.81, 0.88, 0.86, 0.84, 0.85, 0.89, 0.86, 0.82, 0.84, 0.83, 0.85, 0.78, 0.81, 0.77, 0.80, 0.83, 0.79, 0.78, 0.81, 0.82, 0.79, 0.76, 0.83, 0.87, 0.85, 0.88, 0.86, 0.80],
+    "Precision": [0.89, 0.85, 0.84, 0.76, 0.80, 0.77, 0.86, 0.79, 0.83, 0.78, 0.88, 0.82, 0.85, 0.77, 0.76, 0.82, 0.80, 0.78, 0.84, 0.82, 0.81, 0.80, 0.85, 0.82, 0.79, 0.81, 0.80, 0.82, 0.75, 0.79, 0.75, 0.78, 0.81, 0.76, 0.76, 0.79, 0.80, 0.77, 0.73, 0.81, 0.85, 0.82, 0.88, 0.86, 0.78]
 }
+
 df = pd.DataFrame(data)
 
 # Stylish DataFrame
